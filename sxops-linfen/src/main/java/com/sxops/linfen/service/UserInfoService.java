c7ea65c31a0b390f4eda12dd4eacb45f37bbe38c
geweiHome@163.com <geweiHome@163.com>
2019-05-14 18:31:20 +0800

    修改项目名称

--- sxops-linfen/src/main/java/com/sxops/linfen/service/UserInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.sxops.linfen.service;
-
-import com.sxops.linfen.dao.model.LUserInfo;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInfoService extends BaseService<LUserInfo> {
-    void insertUserInfo(LUserInfo userInfo);
-}
